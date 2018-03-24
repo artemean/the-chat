eea2330e4698acd4fb32a90401c525e20fdd6674
Andrii Artemenko1 <Andrii_Artemenko1@epam.com>
2018-03-24 16:13:21 +0100

    Add eslint and prop-types

--- src/components/ChatBox.jsx
+++ src/components/ChatBox.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
-
-function renderMessages(messages) {
-  return messages.map((message, i) => {
-    const text = typeof message === 'string' ? message : message.message;
-    return <div className="chat-box__message" key={i}>{text}</div>
-  })
-}
+import PropTypes from 'prop-types';
 
 function ChatBox(props) {
+  function renderMessages(messages) {
+    return messages.map((message, i) => {
+      console.log(message);
+      const text = typeof message === 'string' ? message : message.message;
+      return <div className="chat-box__message" key={i}>{text}</div>;
+    });
+  }
+
   return (
     <div className="chat-box">
-      {/*<div className="chat-box__user-count">People in chat: {props.numUsers}</div>*/}
+      {/* <div className="chat-box__user-count">People in chat: {props.numUsers}</div> */}
       <div className="chat-box__messages">
         {renderMessages(props.messages)}
       </div>
@@ -21,7 +23,21 @@ function ChatBox(props) {
         value={props.currentMessage}
       />
     </div>
-  )
+  );
 }
+
+ChatBox.propTypes = {
+  messages: PropTypes.arrayOf(PropTypes.string),
+  onMessageChange: PropTypes.func,
+  onMessageKeyDown: PropTypes.func,
+  currentMessage: PropTypes.func,
+};
+
+ChatBox.defaultProps = {
+  messages: [],
+  onMessageChange: () => {},
+  onMessageKeyDown: () => {},
+  currentMessage: '',
+};
 
 export default ChatBox;
--- src/components/LoginBox.jsx
+++ src/components/LoginBox.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function LoginBox(props) {
   return (
@@ -6,7 +7,17 @@ function LoginBox(props) {
       <h1>Please introduce yourself</h1>
       <input type="text" onChange={props.onLoginChange} onKeyDown={props.onLoginKeyDown} />
     </div>
-  )
+  );
 }
+
+LoginBox.propTypes = {
+  onLoginChange: PropTypes.func,
+  onLoginKeyDown: PropTypes.func,
+};
+
+LoginBox.defaultProps = {
+  onLoginChange: () => {},
+  onLoginKeyDown: () => {},
+};
 
 export default LoginBox;
